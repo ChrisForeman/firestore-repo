aa8e59d35f870c45667b8cffeb002443ece39756
Chris <kissthecoder@gmail.com>
2022-05-16 00:17:18 -0400

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 
 import { UnitOfWorkDefault, UnitOfWork } from "./unit-of-work"
 import { BaseRepo } from "./repository"
+import { Outbox } from "./outbox/outbox"
+import { Inbox } from "./inbox/inbox"
+import { DatabaseContext } from "./database-context"
+import { OutboxWorker, EventDTO } from "./outbox/worker"
+import { FireDocument } from "./types"
 
 export {
     UnitOfWork,
     UnitOfWorkDefault,
-    BaseRepo
+    BaseRepo,
+    DatabaseContext,
+    Outbox,
+    Inbox,
+    EventDTO,
+    OutboxWorker,
+    FireDocument
 }
